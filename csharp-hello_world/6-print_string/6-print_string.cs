e45eda716312ef8eff0b9cb71d90860f0359c7d5
Maxified <a.emoh@alustudent.com>
2026-01-18 22:44:46 +0200

    Refactor string output in Main method to use string formatting

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -2,10 +2,10 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        string str = "Holberton School";
-        Console.WriteLine(str + str + str);
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        Console.WriteLine("{0}{0}{0}", str);
         Console.WriteLine(str.Substring(0, 9));
     }
 }
