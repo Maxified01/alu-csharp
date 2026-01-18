d8c33585e7882a731ad8453cbca6451b7463fc16
Maxified <a.emoh@alustudent.com>
2026-01-18 21:41:54 +0200

    Add initial project files and configurations for 7-concat application
    - Created 7-concat.dll and 7-concat.ref.dll for the project.
    - Added project.assets.json to manage dependencies and project settings.
    - Included project.nuget.cache for NuGet package management.
    - Established reference assemblies for .NET 7.0.

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "Holberton";
+        string str2 = "School";
+        Console.WriteLine("Welcome to " + str1 + " " + str2 + "!");
+    }
+}
+
