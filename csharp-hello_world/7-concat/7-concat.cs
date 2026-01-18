fd695c2a8dd6570d123ea51fbd01b4b2c0865aee
Maxified <a.emoh@alustudent.com>
2026-01-18 22:46:33 +0200

    Refactor Main method to accept arguments and improve string concatenation

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -2,11 +2,11 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        string str1 = "Holberton";
-        string str2 = "School";
-        Console.WriteLine("Welcome to " + str1 + " " + str2 + "!");
-    }
-}
-
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		str1 = str1 + " " + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
+	}
+} 
