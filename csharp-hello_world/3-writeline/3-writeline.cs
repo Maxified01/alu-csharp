8cd4bb04f74f9addbfd23e6935777bf01b29823c
Maxified <a.emoh@alustudent.com>
2026-02-02 18:18:35 +0200

    Modified

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -2,9 +2,8 @@
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Programming is like building a multilingual puzzle");
+        Console.WriteLine("\"Programming is like building a multilingual puzzle");
     }
-}
-
+} 
