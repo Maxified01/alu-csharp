fd8d480ee3b52edb9818453fe53eccc97bfc8f92
Maxified01 <a.emoh@alustudent.com>
2026-03-11 09:57:00 +0200

    Dog is an Animal

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+/// <summary>
+/// Represents a base class Animal.
+/// </summary>
+public class Animal
+{
+    // Empty class
+}
+
+/// <summary>
+/// Represents a derived class Dog that inherits from Animal.
+/// </summary>
+public class Dog : Animal
+{
+    // Empty class
+}
